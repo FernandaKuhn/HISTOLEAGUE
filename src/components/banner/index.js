bba140d296633789e323f9f4f3e7228e3570ef04
Fernanda Kuhn <fernandakuhn1@hotmail.com>
2025-04-22 21:57:47 -0300

    feat/banner-animation

--- src/components/banner/index.js
+++ src/components/banner/index.js
@@ -1,23 +1,26 @@
 import styles from "./Banner.module.css";
-import histolol from "../../assets/histolol.png";
+import histolol from "../../assets/1.png";
+import segundaImagem from "../../assets/2.png";
+import terceiraImagem from "../../assets/3.png";
+import quartaImagem from "../../assets/4.png";
+import quintaImagem from "../../assets/5.png";
 
 export default function Banner() {
+  const imagens = [histolol, segundaImagem, terceiraImagem, quartaImagem, quartaImagem];
+
   return (
     <div className={styles.banner}>
-      <div className={styles.content}>
-        <h1 className={styles.title}>
-          Titulo Banner teste
-        </h1>
-        <p className={styles.text}>
-          frase
-        </p>
-      </div>
+      <div className={styles.carousel}>
+        {imagens.map((imagem, index) => (
+          <img
+            key={index}
+            className={styles.image}
+            src={imagem}
+            alt={`Banner ${index + 1}`}
+          />
 
-      <img
-        className={styles.image}
-        src={histolol}
-        alt="Ganhadores do campeonato brasileiro de LOL"
-      />
+        ))}
+      </div>
     </div>
   );
 }
