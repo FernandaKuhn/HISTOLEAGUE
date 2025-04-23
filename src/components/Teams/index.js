57241b6a14d0417071b691446c1067b31ccca83b
Fernanda Kuhn <fernandakuhn1@hotmail.com>
2025-04-23 00:33:22 -0300

    feat/add-title-team-ltasul

--- src/components/Teams/index.js
+++ src/components/Teams/index.js
@@ -4,14 +4,12 @@ export default function Teams({ team }) {
     return (
         <div className={styles.Teams}>
             <img
-                className={styles.Teams}
+                className={styles.img}
                 src={`/assets/team/${team.id}.png`}
                 alt={`Imagem de capa do time ${team.titulo}`}
             />
-
-            <h2 className={styles.title}>{team.titulo}
-                <button className={styles.button}>Ler</button>
-            </h2>
+            <h2 className={styles.title}>{team.titulo}</h2>
+            <button className={styles.button}>Ler</button>
         </div>
     );
 }
--- src/pages/home/index.js
+++ src/pages/home/index.js
@@ -8,11 +8,17 @@ export default function Home() {
     return (
         <main >
             <Banner />
-
+            <div>
+                <h3>LTA SUL - Times</h3>
+                <p className={styles.description}>
+                    Agora o campeonato não é composto apenas por times brasileiros.
+                    A LTA Sul reúne equipes dos países da América do Sul, elevando ainda mais o nível de competitividade no cenário de League of Legends.
+                </p>
+            </div>
             <ul className={styles.team}>
                 {team.map((team) => (
                     <li key={team.id}>
-                        <Teams team={team}/>
+                        <Teams team={team} />
                     </li>
                 ))}
             </ul>
