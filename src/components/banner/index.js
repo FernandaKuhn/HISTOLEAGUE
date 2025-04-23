fced44c27f9a0bd2e56eb7f1b75d57749ea393be
Fernanda Kuhn <fernandakuhn1@hotmail.com>
2025-04-22 22:44:40 -0300

    feat/add-title-banner

--- src/components/banner/index.js
+++ src/components/banner/index.js
@@ -1,9 +1,9 @@
 import styles from "./Banner.module.css";
-import histolol from "../../assets/1.png";
-import segundaImagem from "../../assets/2.png";
-import terceiraImagem from "../../assets/3.png";
-import quartaImagem from "../../assets/4.png";
-import quintaImagem from "../../assets/5.png";
+import histolol from "../../assets/banner/1.png";
+import segundaImagem from "../../assets/banner/2.png";
+import terceiraImagem from "../../assets/banner/3.png";
+import quartaImagem from "../../assets/banner/4.png";
+import quintaImagem from "../../assets/banner/5.png";
 
 export default function Banner() {
   const imagens = [histolol, segundaImagem, terceiraImagem, quartaImagem, quintaImagem];
@@ -20,6 +20,13 @@ export default function Banner() {
           />
 
         ))}
+
+      </div>
+      <div className={styles.container}>
+        <h1 className={styles.titulo}>HISTOLEAGUE</h1>
+        <h3 className={styles.subtitulo}>
+          Reviva os momentos que transformaram jogadores em heróis.
+        </h3>
       </div>
     </div>
   );
--- src/pages/home/index.js
+++ src/pages/home/index.js
@@ -1,10 +1,9 @@
 import Banner from "components/banner";
 
-export default function Home(){
-    return(
-        <main>
-        <Banner/>
-        <h1>Home</h1>
+export default function Home() {
+    return (
+        <main >
+            <Banner />
         </main>
 
     )
